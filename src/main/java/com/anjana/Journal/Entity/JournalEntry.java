503ff59ccda68246695aae991a2cadc2342973ae
anjana732 <anjanasingh1257@gmail.com>
2025-07-15 13:24:51 +0530

    Add User Model

--- src/main/java/com/anjana/Journal/Entity/JournalEntry.java
+++ src/main/java/com/anjana/Journal/Entity/JournalEntry.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Document(collection = "journal_entries")
+//@Document(collection = "journal_entries")
 public class JournalEntry {
 
     @Id
---
+++ src/main/java/com/anjana/Journal/model/User.java
@@ -0,0 +1,4 @@
+package com.anjana.Journal.model;
+
+public class User {
+}
