33a917b7912bf42af5e802efd663b6d0c6c2e6f4
anjana732 <anjanasingh1257@gmail.com>
2025-07-15 13:57:15 +0530

    Setup Lombok

--- src/main/java/com/anjana/Journal/model/User.java
+++ src/main/java/com/anjana/Journal/model/User.java
@@ -1,4 +1,23 @@
 package com.anjana.Journal.model;
 
+import jakarta.annotation.Nonnull;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "users")
+@Data
 public class User {
+
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @Nonnull
+    private String username;
+    @Nonnull
+    private String password;
+
 }
