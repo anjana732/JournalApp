9596adc09462317c722dbca3eabe5244b5db23d6
anjana732 <anjanasingh1257@gmail.com>
2025-07-16 19:49:20 +0530

    Add ArrayList

--- src/main/java/com/anjana/Journal/model/User.java
+++ src/main/java/com/anjana/Journal/model/User.java
@@ -1,11 +1,15 @@
 package com.anjana.Journal.model;
 
+import com.anjana.Journal.Entity.JournalEntry;
 import jakarta.annotation.Nonnull;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+import java.util.ArrayList;
 
 
 @Document(collection = "users")
@@ -20,4 +24,5 @@ public class User {
     @Nonnull
     private String password;
 
+    private List<JournalEntry> journalEntries = new ArrayList<>();
 }
