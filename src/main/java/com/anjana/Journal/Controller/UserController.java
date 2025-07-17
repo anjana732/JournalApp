04bccc53a9ca9a9dc852a8f0d202b5ccba0242f4
anjana732 <anjanasingh1257@gmail.com>
2025-07-17 06:52:34 +0530

    Add user service

--- src/main/java/com/anjana/Journal/Controller/UserController.java
+++ src/main/java/com/anjana/Journal/Controller/UserController.java
@@ -1,4 +1,11 @@
 package com.anjana.Journal.Controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
---
+++ src/main/java/com/anjana/Journal/Services/UserService.java
@@ -0,0 +1,35 @@
+package com.anjana.Journal.Services;
+
+import com.anjana.Journal.Entity.User;
+import com.anjana.Journal.Repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //To save user in database
+    public void saveEntry(User user){
+        userRepository.save(user);
+    }
+
+    //To get all users from database
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    //To get user by Id
+    public Optional<User> findById(ObjectId id){
+        return userRepository.findById(id);
+    }
+
+    //To delete user by Id
+
+    public void deleteById(ObjectId id){
+        userRepository.deleteById(id);
+    }
+
+}
