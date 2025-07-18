0115ec0b7fd22f205920d9ec4cb5405b2c06ee04
anjana732 <anjanasingh1257@gmail.com>
2025-07-18 11:28:12 +0530

    Add spring security

---
+++ src/main/java/com/anjana/Journal/config/SpringSecurity.java
@@ -0,0 +1,23 @@
+package com.anjana.Journal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+
+public class SpringSecurity{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
