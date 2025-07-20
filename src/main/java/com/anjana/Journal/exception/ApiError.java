66ec5afdc556a687ef385d9dd1d53758a7ee00e7
anjana732 <anjanasingh1257@gmail.com>
2025-07-20 07:11:46 +0530

    Add APIError

--- src/main/java/com/anjana/Journal/exception/ApiError.java
+++ src/main/java/com/anjana/Journal/exception/ApiError.java
@@ -1,4 +1,25 @@
 package com.anjana.Journal.exception;
 
 public class ApiError {
+    private String message;
+    private Object details;
+    private int status;
+
+    public ApiError(String message, Object details, int status){
+        this.message = message;
+        this.details = details;
+        this.status = status;
+    };
+
+    public String getMessage(){
+        return message;
+    }
+
+    public Object getDetails(){
+        return details;
+    }
+
+    public int getStatus(){
+        return status;
+    }
 }
